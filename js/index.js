493784db30196dda2e112952c7d4e4b33d3d2c60
= <nathaniel.risum2@gmail.com>
2024-10-01 13:45:35 +0200

    Solve first assignment

--- js/index.js
+++ js/index.js
@@ -1,2 +1,19 @@
 //Write your Javascript code here
-console.log("Shoppinglist")
+console.log("Shoppinglist");
+
+function addNewLiElement(temp) {
+
+    let liElem = document.createElement("li");
+    liElem.setAttribute("id", "swagger");
+    if (temp == "healthy") {
+        liElem.className = "healthy";
+        
+
+    } else {
+        liElem.className = "unhealthy"; 
+    }
+
+    let textNode = document.createTextNode("Anders!");
+    liElem.appendChild(textNode); 
+    return liElem;  
+}
